aa9e34909526887e3153c13e6ecda8258a0aecc9
Simone Saputo <simonesaputo0@gmail.com>
2022-06-29 10:17:00 +0200

    adjustment

--- Palidroma/asset/js/main.js
+++ Palidroma/asset/js/main.js
@@ -1,29 +1,11 @@
-// Palidroma
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
-
-let parolaInserita = prompt('Inserisci una parola: ');
-console.log(parolaInserita);
-
-// definisco la funzione personalizzata
-function inversioneStringa(str) {
-    var arrayDiSottostringhe = str.split(""); // con split spezzetto la stringa sorgente in sottostringhe
-    var reverseArray = arrayDiSottostringhe.reverse(); //reverse mi restituisce le stringhe in ordine inverso
-    return reverseArray.join(""); // il metodo join concatena le sottostringhe in unica stringa
+let parolaInserita = prompt("inserisci una parola, e io ti dirò se è palindroma!");
+function revString(parolaInserita) {
+    var arrayDiSottostringhe = parolaInserita.split("");
+    var reverseArray = arrayDiSottostringhe.reverse();
+    return reverseArray.join("");
 }
-
-document.getElementById('box').innerHTML = `<div>Ok amico, questa è la parola da te inserita ma al contrario... Ti sembra palindroma? <span style="font-size:40px;color:red;background-color:blue;">${inversioneStringa(parolaInserita)}</span></div>`;
-
-
-siNo=prompt('Si o No?')
-if(siNo===String('si')) {
-    document.getElementById('box2').innerHTML = `<div>Esatto amico! la parola <span style="font-size:40px;color:red;background-color:blue;">${inversioneStringa(parolaInserita)}</span> è palindroma.</div>`;
+if (revString(parolaInserita) == parolaInserita){
+    document.getElementById("box").innerHTML += `La parola ${parolaInserita} è palindroma!`
 } else {
-    document.getElementById('box').innerHTML += `<div>Esatto amico, la parola  <span style="font-size:40px;color:red;background-color:blue;">${inversioneStringa(parolaInserita)}</span> non è affatto palindroma.</div>`;
+    document.getElementById("box").innerHTML += `La parola ${parolaInserita} non è palindroma!`
 }
-
-
-
-
-
-
--- Pari-e-Dispari/asset/js/main.js
+++ Pari-e-Dispari/asset/js/main.js
@@ -1,118 +1,29 @@
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
-// Consigli del giorno
-// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
-// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+let pariDispari = prompt("Pari o Dispari?");
+let numeroUtente = parseInt(prompt("Scegli un numero tra 1 & 5"))
+let print = document.getElementById("print");
 
-let bottonePari = document.getElementById('pari');
-let bottoneDispari = document.getElementById('dispari');
-
-
-bottonePari.addEventListener('click',
-  function() {
-    var numeroUtentePari = Number(prompt('Inserisci un numero da 1 a 5'));
-    let arrayPari = [];
-    arrayPari.push(numeroUtentePari);
-    console.log(arrayPari);
-    return arrayPari;
+while ( numeroUtente < 1 || numeroUtente > 5){
+    numeroUtente = parseInt(prompt("Ripeti, il numero deve essere compreso tra 1 e 5"))
 }
-)
+
+var numero;
+function numeroRandom (numero){
+    numero = Math.floor(Math.random()*5 +1);
+    return numero;
+}
+let random = numeroRandom(numero)
+let somma = numeroUtente + random;
 
 
-bottoneDispari.addEventListener('click',
-    function() {
-      var numeroUtenteDispari = Number(prompt('Inserisci un numero da 1 a 5'));
-      let arrayDispari = [];
-      arrayDispari.push(numeroUtenteDispari);
-      console.log(arrayDispari);
-      return arrayDispari;
+
+print.innerHTML += `${getPariDispari()} <div>Il computer ha scelto: ${random}</div><div>L'utente ha scelto: ${numeroUtente}</div>`
+function getPariDispari(){
+    
+    if(pariDispari == "pari" && somma % 2 == 0){
+        return print.innerHTML += `Che fortuna!! Hai vinto, il numero ${somma} è pari!`
+    } else if (pariDispari == "dispari" && somma % 2 != 0){
+        return print.innerHTML += `Che fortuna!! Hai vinto, il numero ${somma} è dispari!`
+    } else {
+        return print.innerHTML += `Stupido umano, il numero ${somma} non è ${pariDispari} hai perso!`
     }
-)
-
-document.getElementById('utente').innerHTML = `<div>L'utente ha scelto il numero: ${arrayPari}</div>`;
-document.getElementById('utente').innerHTML = `<div>L'utente ha scelto il numero: ${arrayDispari}</div>`;
-
-let numero = Math.floor(Math.random() * 5);
-
-// Genero il numero random del computer
-function numRandom(numero) {
-  let x = Math.floor(Math.random() * numero);
-    return x;
 }
-
-document.getElementById('computer').innerHTML += `<div>Il computer ha scelto il numero: ${numRandom(numero)} </div>`;
-
-let somma = arrayPari + arrayDispari;
-
-function pariDispari () {
-        if (numero % 2 == 0) {
-            return 'pari';
-        } else {
-            return 'dispari';
-        }
-}
-
-document.getElementById('vincitore').innerHTML = `${pariDispari(somma)}`;
- 
-    
-
-
-
-// function numRandom (numero) {
-//     let x = Math.floor(Math.random() * numero);
-//     return x;
-// }
-
-// console.log(numRandom (numero));
-
-
-
-// let i = 1;
-
-// while (i <= 10) {
-//     document.getElementById('box').innerHTML += `<div> ${numRandom(numero)} </div>`;
-//     i++;
-// }
-    
-    
-    // let numero = parseInt(prompt('inserisci un numero'));
-    // // invoco la funzione
-    // console.log(pariDispari(numero));
-    
-    // if (pariDispari(numero) == 'pari') {
-    //     document.getElementById('box').innerHTML = `${numero} è pari`;
-    // } else {
-    //     document.getElementById('box').innerHTML = `${numero} è dispari`;
-    // }
-
-    // let numero = parseInt(prompt('inserisci un numero'));
-// // invoco la funzione
-// console.log(pariDispari(numero));
-
-// if (pariDispari(numero) == 'pari') {
-//     document.getElementById('box').innerHTML = `${numero} è pari`;
-// } else {
-//     document.getElementById('box').innerHTML = `${numero} è dispari`;
-// }
-
-
-// var min = 8;
-// var max = 30;
-
-// alertify.prompt( 'Prompt Title', 'Prompt Message', 'Prompt Value',
-//   function(evt, value) {
-//     if (value < min) {
-//       console.log("your value is too low");
-//     } else if (value > max) {
-//       console.log("your value is too large");
-//     } else {
-//       alertify.success('You entered: ' + value)
-//     }
-//   },
-//   function() {
-//     alertify.error('Cancel')
-//   }).setting({type: 'number'});
